f2b8884f499e9aef7028de14bbd15fb15a65f3c8
Lázár Zsolt <lazarzsolti15@gmail.com>
2020-02-17 09:40:39 +0100

    Add project files.

---
+++ github/Autok.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github
+{
+    class Autok
+    {
+        private List<Autok> lista;
+      
+            private string _marka;
+            private string _modell;
+            private int _evjarat;
+            private int _kilometer;
+            private string _uzemanyag;
+            private int _ar;
+
+        public override string ToString()
+        {
+            return (_marka + " " + _modell + " " + _evjarat + " " + _kilometer + " " + _uzemanyag + " " + _ar);
+        }
+
+        public Autok(string marka, string modell, int evjarat, int kilometer, string uzemanyag, int ar)
+        {
+            _marka = marka;
+            _modell = modell;
+            _evjarat = evjarat;
+            _kilometer = kilometer;
+            _uzemanyag = uzemanyag;
+            _ar = ar;
+        }
+
+    }
+}
+
---
+++ github/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (StreamReader rdm = new StreamReader("auto.csv"))
+            {
+                String line = rdm.ReadToEnd();
+                Console.WriteLine(line);
+            }
+            
+
+
+
+
+                Console.ReadKey();
+        }
+    }
+}
